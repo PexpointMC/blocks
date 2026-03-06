e1a0284d9dae912421640345d2adb679c3a3fdf1
Pexpoint <129668993+PexpointMC@users.noreply.github.com>
2026-03-06 12:48:29 -0500

    Update and rename optimiz to optimiz.js

--- optimiz.js
+++ optimiz.js
@@ -1,2 +1,3 @@
 /* Optimiz is a InjectMe and InjectU code. To run on InjectMe, you must translate this code from InjectU to InjectMe, however it is 100% Compatible.
 Created by PexpointMC & CanvasOne */
+
